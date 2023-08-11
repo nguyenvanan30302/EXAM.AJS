622d84cc1c68fc0d546eb0620c9bee9e778d7c14
nguyenvanan30302 <118425891+nguyenvanan30302@users.noreply.github.com>
2023-08-12 02:14:15 +0700

    Update products.ts

---
+++ products.ts
@@ -0,0 +1,9 @@
+// products.ts
+
+export const products = [
+    { id: 1, name: 'Product A', category: 'Category 1', price: 100, imageUrl: 'path/to/image1.jpg' },
+    { id: 2, name: 'Product B', category: 'Category 2', price: 150, imageUrl: 'path/to/image2.jpg' },
+    { id: 3, name: 'Product C', category: 'Category 1', price: 200, imageUrl: 'path/to/image3.jpg' },
+    // ...more products
+  ];
+  
